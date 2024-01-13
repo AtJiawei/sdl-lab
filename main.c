e43f9f0faf8add107cc28950893bc81e8d46e048
AtJiawei <jiaweitian92@gmail.com>
2024-01-13 22:25:24 +0100

    Add 2 paddles and 1 middle line

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include <SDL2/SDL.h> 
 #include "./constants.h"
 
+#define PADDEL_WIDTH 7
+#define PADDEL_HEIGHT 70
+#define PADDEL_INI_X 20
+#define PADDEL_INI_Y 250
+#define PADDEL_INI_V 0
+
 ///////////////////////////////////////////////////////////////////////////////
 // Global variables
 ///////////////////////////////////////////////////////////////////////////////
@@ -21,7 +27,7 @@ struct game_object {
     float height;
     float vel_x;
     float vel_y;
-} ball, paddle;
+} ball, paddle1, paddle2;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Function to initialize our SDL window
@@ -32,7 +38,7 @@ int initialize_window(void) {
         return false;
     }
     window = SDL_CreateWindow(
-        "A simple game loop using C & SDL",
+        "Pong game using C & SDL",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH,
@@ -84,10 +90,20 @@ void setup(void) {
 
 
     // Initialize the paddle 1 object's position
-    // paddle1.x = 
+    paddle1.x = PADDEL_INI_X;
+    paddle1.y = PADDEL_INI_Y;
+    paddle1.width = PADDEL_WIDTH;
+    paddle1.height = PADDEL_HEIGHT;
+    paddle1.vel_x = 0;
+    paddle1.vel_y = 0;
 
     // Initialize the paddle 2 object's position
-
+    paddle2.x = WINDOW_WIDTH - PADDEL_INI_X - PADDEL_WIDTH;
+    paddle2.y = PADDEL_INI_Y;
+    paddle2.width = PADDEL_WIDTH;
+    paddle2.height = PADDEL_HEIGHT;
+    paddle2.vel_x = 0;
+    paddle2.vel_y = 0;
 
 }
 
@@ -96,30 +112,26 @@ void setup(void) {
 ///////////////////////////////////////////////////////////////////////////////
 void update(void) {
     // Get delta_time factor converted to seconds to be used to update objects
-    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
+    float delta_time = (SDL_GetTicks64() - last_frame_time) / 1000.0;
 
     // Store the milliseconds of the current frame to be used in the next one
-    last_frame_time = SDL_GetTicks();
+    last_frame_time = SDL_GetTicks64();
 
     // Move ball as a function of delta time
     ball.x += ball.vel_x * delta_time;
     ball.y += ball.vel_y * delta_time;
 
     // Check for ball collision with the window borders
-    if (ball.x < 0) {
-        ball.x = 0;
+    if (ball.x <= 0) {
         ball.vel_x = -ball.vel_x;
     }
-    if (ball.x + ball.height > WINDOW_WIDTH) {
-        ball.x = WINDOW_WIDTH - ball.width;
+    if (ball.x + ball.height >= WINDOW_WIDTH) {
         ball.vel_x = -ball.vel_x;
     }
-    if (ball.y < 0) {
-        ball.y = 0;
+    if (ball.y <= 0) {
         ball.vel_y = -ball.vel_y;
     }
-    if (ball.y + ball.height > WINDOW_HEIGHT) {
-        ball.y = WINDOW_HEIGHT - ball.height;
+    if (ball.y + ball.height >= WINDOW_HEIGHT) {
         ball.vel_y = -ball.vel_y;
     }
 }
@@ -142,9 +154,33 @@ void render(void) {
     SDL_RenderFillRect(renderer, &ball_rect);
 
 
-    // Draw a rentangle for the line
+    // Draw a line in the middle of the renderer
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderDrawLine(renderer, WINDOW_WIDTH/2, 0, WINDOW_WIDTH/2, WINDOW_HEIGHT);
+
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    
+    // Draw a rectangle for the left paddle as paddle1 
+    SDL_Rect paddle1_rect = {
+        (int)paddle1.x,
+        (int)paddle1.y,
+        (int)paddle1.width,
+        (int)paddle1.height
+    };
+
+     // Draw a rectangle for the left paddle as paddle1 
+    SDL_Rect paddle2_rect = {
+        (int)paddle2.x,
+        (int)paddle2.y,
+        (int)paddle2.width,
+        (int)paddle2.height
+    };
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // sets the draw color
+    SDL_RenderFillRect(renderer, &paddle1_rect);
+    SDL_RenderFillRect(renderer, &paddle2_rect);
+
+
     SDL_RenderPresent(renderer); // double buffer. This is to present the hidden buffer to the screen / makes the updated rendering target visible to the
 }
 
